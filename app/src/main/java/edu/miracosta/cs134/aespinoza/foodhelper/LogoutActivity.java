8b336d3ac852d2e1a07547370fb62387b898cbd8
Alfredo Hernandez Jr <54375363+AlfredoHernandezJr@users.noreply.github.com>
2020-05-21 15:40:14 -0700

    Added a lot of stuff for settings.

--- app/src/main/java/edu/miracosta/cs134/aespinoza/foodhelper/LogoutActivity.java
+++ app/src/main/java/edu/miracosta/cs134/aespinoza/foodhelper/LogoutActivity.java
@@ -2,13 +2,42 @@ package edu.miracosta.cs134.aespinoza.foodhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LogoutActivity extends AppCompatActivity {
+
+    // Frame Animation
+    AnimationDrawable frameAnim ; // frameAnim == null
+    private ImageView porkyImageView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
+
+        porkyImageView = findViewById(R.id.porkieImageView) ;
+
+        if(frameAnim == null)
+        {
+            porkyImageView.setBackgroundResource(R.drawable.frame_anim) ;
+            frameAnim = (AnimationDrawable) porkyImageView.getBackground() ;
+        }
+        // If the animation is running (and we click button), stop the animation
+        if(frameAnim.isRunning())
+        {
+            frameAnim.stop() ;
+        }
+        else
+        {
+            frameAnim.start() ;
+        }
+    }
+
+    public void toggleFrameAnim(View v)
+    {
+        // Won't need, but just in case.
     }
 }
--- app/src/main/java/edu/miracosta/cs134/aespinoza/foodhelper/SettingsActivity.java
+++ app/src/main/java/edu/miracosta/cs134/aespinoza/foodhelper/SettingsActivity.java
@@ -3,12 +3,26 @@ package edu.miracosta.cs134.aespinoza.foodhelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content,new SettingsActivityFragment())
+                .commit();
+    }
+
+    public static class SettingsActivityFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+
+        }
     }
 }
